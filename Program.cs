a5d8ac31ee7551a971145a226d8baddae06c1731
Mrliduanyang <liduanyangchina@gmail.com>
2020-09-06 19:08:33 +0800

    2020-09-06

--- Program.cs
+++ Program.cs
@@ -475,6 +475,30 @@ namespace FuckingAlgorithm {
                 left = left.next;
                 return res;
             }
+
+            public static int trap(int[] height) {
+                if (height.Length == 0) {
+                    return 0;
+                }
+                int n = height.Length;
+                int ans = 0;
+                int[] l_max = new int[n];
+                int[] r_max = new int[n];
+                // base
+                l_max[0] = height[0];
+                r_max[n - 1] = height[n - 1];
+                // 从左向右计算 l_max
+                for (int i = 1; i < n; i++) {
+                    l_max[i] = Math.Max(height[i], l_max[i - 1]);
+                }
+                // 从右向左计算 r_max
+                for (int i = n - 2; i >= 0; i--) {
+                    r_max[i] = Math.Max(height[i], r_max[i + 1]);
+                }
+                for (int i = 1; i < n - 1; i++)
+                    ans += Math.Min(l_max[i], r_max[i]) - height[i];
+                return ans;
+            }
         }
 
         class DataStructure {
