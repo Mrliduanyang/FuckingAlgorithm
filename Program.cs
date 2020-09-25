de1f78b1a5ec1fff0d5f03f547b87f47e9140505
Mrliduanyang <liduanyangchina@gmail.com>
2020-09-25 18:04:44 +0800

    2020-09-25

--- Program.cs
+++ Program.cs
@@ -1364,7 +1364,68 @@ namespace FuckingAlgorithm {
                 return Helper(nums, 0, nums.Length - 1);
             }
 
-            // 括号的题还有两个没做
+            public static TreeNode BuildTree(int[] preorder, int[] inorder) {
+                TreeNode build(int[] preorder, int preStart, int preEnd,
+                    int[] inorder, int inStart, int inEnd) {
+                    if (preStart > preEnd) {
+                        return null;
+                    }
+                    // root 节点对应的值就是前序遍历数组的第一个元素
+                    int rootVal = preorder[preStart];
+                    // rootVal 在中序遍历数组中的索引
+                    int index = 0;
+                    for (int i = inStart; i <= inEnd; i++) {
+                        if (inorder[i] == rootVal) {
+                            index = i;
+                            break;
+                        }
+                    }
+
+                    int leftSize = index - inStart;
+
+                    // 先构造出当前根节点
+                    TreeNode root = new TreeNode(rootVal);
+                    // 递归构造左右子树
+                    root.left = build(preorder, preStart + 1, preStart + leftSize,
+                        inorder, inStart, index - 1);
+
+                    root.right = build(preorder, preStart + leftSize + 1, preEnd,
+                        inorder, index + 1, inEnd);
+                    return root;
+                }
+                return build(preorder, 0, preorder.Length - 1,
+                    inorder, 0, inorder.Length - 1);
+            }
+
+            public static TreeNode BuildTree_1(int[] inorder, int[] postorder) {
+                TreeNode build(int[] inorder, int inStart, int inEnd, int[] postorder, int postStart, int postEnd) {
+                    if (inStart > inEnd) {
+                        return null;
+                    }
+                    // root 节点对应的值就是后序遍历数组的最后一个元素
+                    int rootVal = postorder[postEnd];
+                    // rootVal 在中序遍历数组中的索引
+                    int index = 0;
+                    for (int i = inStart; i <= inEnd; i++) {
+                        if (inorder[i] == rootVal) {
+                            index = i;
+                            break;
+                        }
+                    }
+                    // 左子树的节点个数
+                    int leftSize = index - inStart;
+                    TreeNode root = new TreeNode(rootVal);
+                    // 递归构造左右子树
+                    root.left = build(inorder, inStart, index - 1,
+                        postorder, postStart, postStart + leftSize - 1);
+
+                    root.right = build(inorder, index + 1, inEnd,
+                        postorder, postStart + leftSize, postEnd - 1);
+                    return root;
+                }
+                return build(inorder, 0, inorder.Length - 1,
+                    postorder, 0, postorder.Length - 1);
+            }
         }
 
         class DataStructure {
