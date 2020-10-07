8bf0764c52523398f9af43ed8d51af2786e04a4e
Mrliduanyang <liduanyangchina@gmail.com>
2020-10-07 18:36:28 +0800

    2020-10-07

--- Program.cs
+++ Program.cs
@@ -1561,6 +1561,29 @@ namespace FuckingAlgorithm {
                 }
                 return new string(stack.ToArray().Reverse().ToArray());
             }
+
+            public void SortColors(int[] nums) {
+                int p0 = 0, curr = 0;
+                int p2 = nums.Length - 1;
+                while (curr <= p2) {
+                    if (nums[curr] == 0) {
+                        int tmp = nums[curr];
+                        nums[curr] = nums[p0];
+                        nums[p0] = tmp;
+
+                        p0++;
+                        curr++;
+                    } else if (nums[curr] == 1) {
+                        curr++;
+                    } else if (nums[curr] == 2) {
+                        int tmp = nums[curr];
+                        nums[curr] = nums[p2];
+                        nums[p2] = tmp;
+
+                        p2--;
+                    }
+                }
+            }
         }
 
         class DataStructure {
