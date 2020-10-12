3cdf53f4f82ccfe051f913c7b152bedaec63c179
Mrliduanyang <liduanyangchina@gmail.com>
2020-10-12 13:09:39 +0800

    2020-10-12

--- Program.cs
+++ Program.cs
@@ -1653,6 +1653,84 @@ namespace FuckingAlgorithm {
                 return ans;
             }
 
+            public int GetMinimumDifference(TreeNode root) {
+                int pre;
+                int ans;
+
+                void DFS(TreeNode root) {
+                    if (root == null) {
+                        return;
+                    }
+                    DFS(root.left);
+                    if (pre == -1) {
+                        pre = root.val;
+                    } else {
+                        ans = Math.Min(ans, root.val - pre);
+                        pre = root.val;
+                    }
+                    DFS(root.right);
+                }
+
+                ans = int.MaxValue;
+                pre = -1;
+                DFS(root);
+                return ans;
+            }
+
+            public List<List<int>> ZigzagLevelOrder(TreeNode root) {
+                var res = new List<List<int>>();
+                if (root == null) return res;
+
+                var queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+                int depth = 1;
+
+                while (queue.Count != 0) {
+                    int count = queue.Count;
+                    var tmp = new List<int>();
+                    for (int i = 0; i < count; i++) {
+                        var node = queue.Dequeue();
+                        if (depth % 2 == 0) {
+                            tmp.Insert(0, node.val);
+
+                        } else {
+                            tmp.Add(node.val);
+                        }
+                        if (node.left != null) {
+                            queue.Enqueue(node.left);
+                        }
+                        if (node.right != null) {
+                            queue.Enqueue(node.right);
+                        }
+                    }
+                    depth++;
+                    res.Add(tmp);
+                }
+                return res;
+            }
+
+            public bool IsValidBST(TreeNode root) {
+                //rot为当前节点值，low为子树下限，up为上限
+                bool Helper(TreeNode rot, int low, int up) {
+                    if (rot == null) {
+                        return true;
+                    }
+                    //当前为左子树时，无下限，上限为根节点
+                    if (!Helper(rot.left, low, rot.val))
+                        return false;
+                    //当前为右子树时，无上限，下限为根节点
+                    if (!Helper(rot.right, rot.val, up))
+                        return false;
+
+                    if (low != -1 && rot.val <= low)
+                        return false;
+                    if (up != -1 && rot.val >= up)
+                        return false;
+
+                    return true;
+                }
+                return Helper(root, -1, -1);
+            }
         }
 
         class DataStructure {
