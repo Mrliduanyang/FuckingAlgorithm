b9b80371b7ab37d47743b48bbc423cd44a249e10
Mrliduanyang <duanyangchn@gmail.com>
2020-10-13 18:34:43 +0800

    2020-10-13

--- Program.cs
+++ Program.cs
@@ -1731,6 +1731,36 @@ namespace FuckingAlgorithm {
                 }
                 return Helper(root, -1, -1);
             }
+
+            public ListNode ReverseKGroup(ListNode head, int k) {
+                ListNode Reverse(ListNode a, ListNode b) {
+                    ListNode pre, cur, nxt;
+                    pre = null;
+                    cur = a;
+                    nxt = a;
+                    // while 终止的条件改一下就行了
+                    while (cur != b) {
+                        nxt = cur.next;
+                        cur.next = pre;
+                        pre = cur;
+                        cur = nxt;
+                    }
+                    // 返回反转后的头结点
+                    return pre;
+                }
+
+                if (head == null) return null;
+
+                ListNode a, b;
+                a = b = head;
+                for (int i = 0; i < k; i++) {
+                    if (b == null) return head;
+                    b = b.next;
+                }
+                var newHead = Reverse(a, b);
+                a.next = ReverseKGroup(b, k);
+                return newHead;
+            }
         }
 
         class DataStructure {
