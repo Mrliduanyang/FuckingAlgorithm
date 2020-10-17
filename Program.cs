c6f9ec6771c18b0dc79639eaf18bc9426104990e
Mrliduanyang <duanyangchn@gmail.com>
2020-10-17 20:36:11 +0800

    2020-10-17

--- Program.cs
+++ Program.cs
@@ -1857,6 +1857,55 @@ namespace FuckingAlgorithm {
                 }
                 return ans;
             }
+
+            public List<List<string>> TotalNQueens(int n) {
+                var res = new List<List<string>>();
+                bool IsValid(char[][] board, int row, int col) {
+                    int n = board.GetLength(0);
+                    // 判断列上是否有皇后
+                    for (int i = 0; i < n; i++) {
+                        if (board[i][col] == 'Q') {
+                            return false;
+                        }
+                    }
+                    // 检查右上方是否有皇后互相冲突
+                    for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+                        if (board[i][j] == 'Q')
+                            return false;
+                    }
+                    // 检查左上方是否有皇后互相冲突
+                    for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+                        if (board[i][j] == 'Q')
+                            return false;
+                    }
+                    return true;
+                }
+                void Backtrack(char[][] board, int row) {
+                    if (row == board.GetLength(0)) {
+                        res.Add(board.Select(item => String.Join("", item)).ToList());
+                        return;
+                    }
+                    int cols = board.GetLength(0);
+                    for (int col = 0; col < cols; col++) {
+                        if (!IsValid(board, row, col)) {
+                            continue;
+                        }
+                        board[row][col] = 'Q';
+                        Backtrack(board, row + 1);
+                        board[row][col] = '.';
+                    }
+                }
+
+                var board = new char[n][];
+                for (int i = 0; i < n; i++) {
+                    var tmp = new char[n];
+                    Array.Fill(tmp, '.');
+                    board[i] = tmp;
+                }
+                Backtrack(board, 0);
+
+                return res;
+            }
         }
 
         class DataStructure {
@@ -2170,8 +2219,7 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            algorithm.CommonChars(new string[] { "bella", "label", "roller" });
-            // System.Console.WriteLine(algorithm.RemoveDuplicateLetters("abbcc"));
+            System.Console.WriteLine(algorithm.TotalNQueens(4));
         }
     }
 }
