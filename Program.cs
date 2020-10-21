032b1a830927041e3eb737b8093aff0ec0750adc
Mrliduanyang <liduanyangchina@gmail.com>
2020-10-21 21:49:32 +0800

    2020-10-21

--- Program.cs
+++ Program.cs
@@ -1978,6 +1978,23 @@ namespace FuckingAlgorithm {
                 MergeList(l1, l2);
             }
 
+            public bool IsLongPressedName(string name, string typed) {
+                int i = 0, j = 0;
+                while (j < typed.Length) {
+                    if (i < name.Length && name[i] == typed[j]) {
+                        i++;
+                        j++;
+                    } else if (j > 0 && typed[j] == typed[j - 1]) {
+                        j++;
+                    } else {
+                        // 除了上面两种情况外，指针不会移动，判定失败
+                        return false;
+                    }
+                }
+                return i == name.Length;
+
+            }
+
         }
 
         class DataStructure {
