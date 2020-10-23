7d49513ab6d90b3ddca964dfad7e35d6f8269527
Mrliduanyang <duanyangchn@gmail.com>
2020-10-23 16:54:11 +0800

    2020-10-23

--- Program.cs
+++ Program.cs
@@ -462,20 +462,19 @@ namespace FuckingAlgorithm {
 
             ListNode left;
             public bool IsPalindrome(ListNode head) {
+                bool Traverse(ListNode right) {
+                    if (right == null) {
+                        return true;
+                    }
+                    // 利用后序遍历，通过递归，深入到链表最后一个节点，然后比较right和left。
+                    // 之后right返回到递归的上一层，相当于右指针左移，left=left.next等于左指针右移，巧妙实现双指针。
+                    bool res = Traverse(right.next);
+                    res = res && (left.val == right.val);
+                    left = left.next;
+                    return res;
+                }
                 left = head;
                 return Traverse(head);
-            }
-
-            public bool Traverse(ListNode right) {
-                if (right == null) {
-                    return true;
-                }
-                // 利用后序遍历，通过递归，深入到链表最后一个节点，然后比较right和left。
-                // 之后right返回到递归的上一层，相当于右指针左移，left=left.next等于左指针右移，巧妙实现双指针。
-                bool res = Traverse(right.next);
-                res = res && (left.val == right.val);
-                left = left.next;
-                return res;
             }
 
             public int Trap_1(int[] height) {
