eedfab436a8e55aeeac008250ee8d4fe7c76a4f1
liduanyang <liduanyang@techfantasy.cn>
2020-11-01 18:51:20 +0800

    2020-11-01

--- Program.cs
+++ Program.cs
@@ -2648,6 +2648,49 @@ namespace FuckingAlgorithm {
                 }
                 return res + need;
             }
+            public int UniquePaths(int m, int n) {
+                var dp = new int[m, n];
+                for (int i = 0; i < m; i++) {
+                    dp[i, 0] = 1;
+                }
+                for (int j = 1; j < n; j++) {
+                    dp[0, j] = 1;
+                }
+                for (int i = 1; i < m; i++) {
+                    for (int j = 1; j < n; j++) {
+                        dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                    }
+                }
+                return dp[m - 1, n - 1];
+            }
+
+            public int UniquePathsWithObstacles(int[][] obstacleGrid) {
+                int m = obstacleGrid.Length;
+                int n = obstacleGrid[0].Length;
+                var dp = new int[m, n];
+                for (int i = 0; i < m; ++i) {
+                    // 如果在第一列遇到障碍，那下面的都无法到达，break即可
+                    if (obstacleGrid[i][0] == 1) break;
+                    dp[i, 0] = 1;
+                }
+
+                for (int i = 0; i < n; ++i) {
+                    if (obstacleGrid[0][i] == 1) break;
+                    dp[0, i] = 1;
+                }
+
+                for (int i = 1; i < m; i++) {
+                    for (int j = 1; j < n; j++) {
+                        if (obstacleGrid[i][j] == 1) {
+                            dp[i, j] = 0;
+                            continue;
+                        } else {
+                            dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+                        }
+                    }
+                }
+                return dp[m - 1, n - 1];
+            }
         }
 
         public class DataStructure {
@@ -2816,7 +2859,7 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            System.Console.WriteLine(algorithm.SubsetsWithDup(new int[] { 4, 4, 4, 1, 4 }));
+            System.Console.WriteLine(algorithm.UniquePaths(7, 3));
         }
     }
 }
