fab5bcd1f39596beae0141731dd826ed58d6b150
Mrliduanyang <liduanyangchina@gmail.com>
2020-11-13 18:10:08 +0800

    2020-11-13

--- Program.cs
+++ Program.cs
@@ -3191,6 +3191,23 @@ namespace FuckingAlgorithm {
                 }
                 return res.Values.ToList();
             }
+
+            public ListNode OddEvenList(ListNode head) {
+                if (head == null) {
+                    return head;
+                }
+                ListNode evenHead = head.next;
+                ListNode odd = head, even = evenHead;
+                while (even != null && even.next != null) {
+                    odd.next = even.next;
+                    odd = odd.next;
+                    even.next = odd.next;
+                    even = even.next;
+                }
+                odd.next = evenHead;
+                return head;
+            }
+
         }
 
         public class DataStructure {
