585480ace8362c22731d0a1eafbd8b4f64af9db9
Mrliduanyang <duanyangchn@gmail.com>
2020-11-26 11:56:01 +0800

    2020-11-26

--- Program.cs
+++ Program.cs
@@ -3946,15 +3946,73 @@ namespace FuckingAlgorithm {
                 dummy.next = head;
                 var prev = dummy;
                 var cur = head;
-                while(cur != null){
-                    if(cur.val == val){
+                while (cur != null) {
+                    if (cur.val == val) {
                         prev.next = cur.next;
-                    }else{
+                    } else {
                         prev = cur;
                     }
                     cur = cur.next;
                 }
                 return dummy.next;
+            }
+
+            public int MaxmiumGap(int[] nums) {
+                int n = nums.Length;
+                if (n < 2) {
+                    return 0;
+                }
+                long exp = 1;
+                var buf = new int[n];
+                var maxVal = nums.Max();
+                // 基数排序，按照每一位数字大小在该位上将元素顺序排列，最后可得到从高位到低位的相对顺序排列
+                while (maxVal >= exp) {
+                    int[] cnt = new int[10];
+                    for (int i = 0; i < n; i++) {
+                        int digit = (nums[i] / (int) exp) % 10;
+                        cnt[digit]++;
+                    }
+                    // 通过将基数索引累加，可以确定元素按基数顺序在数组中的位置
+                    for (int i = 1; i < 10; i++) {
+                        cnt[i] += cnt[i - 1];
+                    }
+                    for (int i = n - 1; i >= 0; i--) {
+                        int digit = (nums[i] / (int) exp) % 10;
+                        buf[cnt[digit] - 1] = nums[i];
+                        cnt[digit]--;
+                    }
+                    buf.CopyTo(nums, 0);
+                    exp *= 10;
+                }
+                int res = 0;
+                for (int i = 1; i < n; i++) {
+                    res = Math.Max(res, nums[i] - nums[i - 1]);
+                }
+                return res;
+            }
+
+            public bool ContainsNearbyDuplicate(int[] nums, int k) {
+                var set = new SortedSet<int>();
+                for (int i = 0; i < nums.Length; ++i) {
+                    if (set.Contains(nums[i])) {
+                        return true;
+                    }
+                    set.Add(nums[i]);
+                    if (set.Count > k) {
+                        set.Remove(nums[i - k]);
+                    }
+                }
+                return false;
+            }
+
+            public bool ContainsDuplicate(int[] nums) {
+                var res = new SortedSet<int>();
+                foreach (var num in nums) {
+                    if (!res.Add(num)) {
+                        return true;
+                    }
+                }
+                return false;
             }
         }
 
@@ -4152,7 +4210,7 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            algorithm.SortString("aaaabbbbcccc");
+            algorithm.ContainsNearbyDuplicate(new int[] { 99, 99 }, 2);
         }
     }
 }
