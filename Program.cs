489d80113a7f29d4c166a18ba366951c23cffa19
Mrliduanyang <duanyangchn@gmail.com>
2020-11-27 20:35:32 +0800

    2020-11-27

--- Program.cs
+++ Program.cs
@@ -4038,6 +4038,100 @@ namespace FuckingAlgorithm {
                 }
                 return false;
             }
+
+            public int FourSumCount(int[] A, int[] B, int[] C, int[] D) {
+                var dict = new Dictionary<int, int>();
+                foreach (var numA in A) {
+                    foreach (var numB in B) {
+                        dict[numA + numB] = dict.GetValueOrDefault(numA + numB, 0) + 1;
+                    }
+                }
+                int ans = 0;
+                foreach (var numC in C) {
+                    foreach (var numD in D) {
+                        if (dict.ContainsKey(-(numC + numD))) {
+                            ans += dict[-(numC + numD)];
+                        }
+                    }
+                }
+                return ans;
+            }
+
+            public int ComputeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+                int area1 = (C - A) * (D - B);
+                int area2 = (G - E) * (H - F);
+
+                if (A >= G || C <= E || B >= H || D <= F)
+                    return area1 + area2;
+
+                int length = Math.Min(C, G) - Math.Max(A, E);
+                int height = Math.Min(D, H) - Math.Max(B, F);
+                int area3 = length * height;
+                return area1 + area2 - area3;
+            }
+
+            public bool CanFinish(int numCourses, int[][] prerequisites) {
+                var indegree = new int[numCourses];
+                var adjacency = new List<List<int>>();
+                var queue = new Queue<int>();
+                for (int i = 0; i < numCourses; i++) {
+                    adjacency.Add(new List<int>());
+                }
+                foreach (var node in prerequisites) {
+                    // node=[0,1]，要学习0，先学习1
+                    indegree[node[0]]++;
+                    adjacency[node[1]].Add(node[0]);
+                }
+                for (int i = 0; i < numCourses; i++) {
+                    // 先找到入度为0的，作为起点
+                    if (indegree[i] == 0) {
+                        queue.Enqueue(i);
+                    }
+                }
+                while (queue.Count != 0) {
+                    int pre = queue.Dequeue();
+                    numCourses--;
+                    foreach (var cur in adjacency[pre]) {
+                        // 入度降为0，说明没有前驱课程
+                        if (--indegree[cur] == 0) {
+                            queue.Enqueue(cur);
+                        }
+                    }
+                }
+                return numCourses == 0;
+            }
+
+            public int[] FindOrder(int numCourses, int[][] prerequisites) {
+                var indegree = new int[numCourses];
+                var adjacency = new List<List<int>>();
+                var queue = new Queue<int>();
+                var res = new List<int>();
+
+                for (int i = 0; i < numCourses; i++) {
+                    adjacency.Add(new List<int>());
+                }
+                foreach (var node in prerequisites) {
+                    indegree[node[0]]++;
+                    adjacency[node[1]].Add(node[0]);
+                }
+                for (int i = 0; i < numCourses; i++) {
+                    if (indegree[i] == 0) {
+                        queue.Enqueue(i);
+                        res.Add(i);
+                    }
+                }
+                while (queue.Count != 0) {
+                    int pre = queue.Dequeue();
+                    numCourses--;
+                    foreach (var cur in adjacency[pre]) {
+                        if (--indegree[cur] == 0) {
+                            queue.Enqueue(cur);
+                            res.Add(cur);
+                        }
+                    }
+                }
+                return numCourses == 0 ? res.ToArray() : new int[] { };
+            }
         }
 
         public class DataStructure {
@@ -4229,6 +4323,33 @@ namespace FuckingAlgorithm {
 
                 public int GetMin() {
                     return minStack.Peek();
+                }
+            }
+
+            public class MyStack {
+                public Queue<int> queue;
+                public MyStack() {
+                    queue = new Queue<int>();
+                }
+
+                public void Push(int x) {
+                    int n = queue.Count;
+                    queue.Enqueue(x);
+                    for (int i = 0; i < n; i++) {
+                        queue.Enqueue(queue.Dequeue());
+                    }
+                }
+
+                public int Pop() {
+                    return queue.Dequeue();
+                }
+
+                public int Top() {
+                    return queue.First();
+                }
+
+                public bool Empty() {
+                    return queue.Count == 0;
                 }
             }
         }
