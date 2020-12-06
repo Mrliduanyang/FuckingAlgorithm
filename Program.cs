a9c8caef18e9a3c66607e883b632324a12292005
Mrliduanyang <liduanyangchina@gmail.com>
2020-12-06 21:32:37 +0800

    2020-12-06

--- Program.cs
+++ Program.cs
@@ -4711,6 +4711,30 @@ namespace FuckingAlgorithm {
             //     return 0;
             // }
 
+            public int MaxAncestorDiff() {
+                var root = new TreeNode(12);
+                if (root == null) return 0;
+                var res = int.MinValue;
+                (int, int) Helper(TreeNode root) {
+                    if (root == null) {
+                        return (100001, -1);
+                    }
+                    var curVal = root.val;
+                    var(lMin, lMax) = Helper(root.left);
+                    var(rMin, rMax) = Helper(root.right);
+                    lMin = lMin == 100001 ? curVal : lMin;
+                    lMax = lMax == -1 ? curVal : lMax;
+                    rMin = rMin == 100001 ? curVal : rMin;
+                    rMax = rMax == -1 ? curVal : rMax;
+                    var curMin = Math.Min(lMin, rMin);
+                    var curMax = Math.Max(lMax, rMax);
+                    res = Math.Max(res, Math.Max(Math.Abs(curVal - curMin), Math.Abs(curVal - curMax)));
+                    return (Math.Min(curVal, curMin), Math.Max(curVal, curMax));
+                }
+                Helper(root);
+                return res;
+            }
+
         }
 
         public class DataStructure {
@@ -4934,6 +4958,7 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
+            algorithm.MaxAncestorDiff();
         }
     }
 }
