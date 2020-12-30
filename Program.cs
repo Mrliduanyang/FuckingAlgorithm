ba1f042e3b94f9cfc0970d28fd8db997bfba6828
Mrliduanyang <liduanyangchina@gmail.com>
2020-12-30 13:58:25 +0800

    2020-12-30

--- Program.cs
+++ Program.cs
@@ -5736,6 +5736,35 @@ namespace FuckingAlgorithm {
                 }
                 return res;
             }
+
+            // #1046
+            class StoneComparer : IComparer {
+                public int Compare(object x, object y) {
+                    var x1 = (int) x;
+                    var y1 = (int) y;
+                    // 大顶堆比较器，大于返回-1
+                    return x1 > y1 ? -1 : 1;
+
+                }
+            }
+
+            public int LastStoneWeight(int[] stones) {
+                var heap = new SortedList(new StoneComparer());
+                foreach (var stone in stones) {
+                    heap.Add(stone, stone);
+                }
+                while (heap.Count > 1) {
+                    var fisrt = (int) heap.GetByIndex(0);
+                    heap.RemoveAt(0);
+                    var second = (int) heap.GetByIndex(0);
+                    heap.RemoveAt(0);
+                    var residual = fisrt - second;
+                    if (residual > 0) {
+                        heap.Add(residual, residual);
+                    }
+                }
+                return heap.Count == 0 ? 0 : (int) heap.GetByIndex(0);
+            }
         }
         public class DataStructure {
             public class LRU {
@@ -6074,13 +6103,7 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            algorithm.FindRelativeRanks(new int[] {
-                10,
-                3,
-                8,
-                9,
-                4
-            });
+            algorithm.LastStoneWeight(new int[] { 2, 7, 4, 1, 8, 1 });
         }
     }
 }
