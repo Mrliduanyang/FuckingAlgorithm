c0ff0639844933d31586cf13d8ded08557364b70
Mrliduanyang <duanyangchn@gmail.com>
2021-01-03 15:46:00 +0800

    update .Net version

--- Program.cs
+++ Program.cs
@@ -6221,7 +6221,7 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            algorithm.NumberOfArithmeticSlices(new int[] { 1, 2, 3, 4 });
+            System.Console.WriteLine(algorithm.NumberOfArithmeticSlices(new int[] { 1, 2, 3, 4 }));
         }
     }
 }
