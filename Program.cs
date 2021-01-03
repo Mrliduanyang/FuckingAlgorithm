d2d459a32d9805d2df8d88cabd871f5f05f04309
Mrliduanyang <duanyangchn@gmail.com>
2021-01-03 20:12:35 +0800

    2021-01-03

--- Program.cs
+++ Program.cs
@@ -81,6 +81,7 @@ namespace FuckingAlgorithm {
                 return (dp[amount] == amount + 1) ? -1 : dp[amount];
             }
 
+            // #46
             public List<int[]> Permute(int[] nums) {
                 // 存储全排列结果
                 List<int[]> res = new List<int[]>();
@@ -5907,6 +5908,24 @@ namespace FuckingAlgorithm {
                     }
                 }
                 return nums[left];
+            }
+
+            // #526
+            public int CountArrangement(int n) {
+                int res = 0;
+                var vis = new bool[n + 1];
+                void Helper(int idx) {
+                    if (idx == n + 1) res++;
+                    for (int i = 1; i <= n; i++) {
+                        if (!vis[i] && (idx % i == 0 || i % idx == 0)) {
+                            vis[i] = true;
+                            Helper(idx + 1);
+                            vis[i] = false;
+                        }
+                    }
+                }
+                Helper(1);
+                return res;
             }
         }
 
