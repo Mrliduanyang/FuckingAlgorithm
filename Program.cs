673e09c4aefea8f59b3bd66b528f88bce592a43b
Mrliduanyang <duanyangchn@gmail.com>
2021-01-04 16:17:56 +0800

    2021-01-04

--- Program.cs
+++ Program.cs
@@ -62,39 +62,26 @@ namespace FuckingAlgorithm {
             }
 
             public int CoinChange(int[] coins, int amount) {
-                    // 当目标金额为i时，至少需要dp[i]枚硬币凑出
-                    // 初始化dp，dp各元素设置为最大值
-                    int[] dp = new int[amount + 1];
-                    Array.Fill(dp, amount + 1);
-                    // base，金额为0，硬币为0
-                    dp[0] = 0;
-                    // 求dp中所有金额
-                    for (int i = 0; i < dp.Length; i++) {
-                        // 遍历可用的硬币
-                        foreach (var coin in coins) {
-                            // 减去硬币面值后，金额小于0，无解跳过
-                            if (i - coin < 0) continue;
-                            // 选择硬币数最少的方案
-                            dp[i] = Math.Min(dp[i], 1 + dp[i - coin]);
-                        }
+                // 当目标金额为i时，至少需要dp[i]枚硬币凑出
+                // 初始化dp，dp各元素设置为最大值
+                int[] dp = new int[amount + 1];
+                Array.Fill(dp, amount + 1);
+                // base，金额为0，硬币为0
+                dp[0] = 0;
+                // 求dp中所有金额
+                for (int i = 0; i < dp.Length; i++) {
+                    // 遍历可用的硬币
+                    foreach (var coin in coins) {
+                        // 减去硬币面值后，金额小于0，无解跳过
+                        if (i - coin < 0) continue;
+                        // 选择硬币数最少的方案
+                        dp[i] = Math.Min(dp[i], 1 + dp[i - coin]);
                     }
-                    return (dp[amount] == amount + 1) ? -1 : dp[amount];
                 }
+                return (dp[amount] == amount + 1) ? -1 : dp[amount];
+            }
 
-                <<
-                <<
-                <<
-                <
-                HEAD ==
-                ==
-                ==
-                =
-                // #46
-                >>
-                >>
-                >>
-                >
-                54 ff6fa7c0caaf1ab18d18db31f46cb9afa02ce6
+            // #46
             public List<int[]> Permute(int[] nums) {
                 // 存储全排列结果
                 List<int[]> res = new List<int[]>();
