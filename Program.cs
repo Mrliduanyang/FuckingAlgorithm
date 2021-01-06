bf2993aafe571eb3d261bc563130eff824289790
Mrliduanyang <duanyangchn@gmail.com>
2021-01-06 21:21:22 +0800

    2021-01-06

--- Program.cs
+++ Program.cs
@@ -5982,6 +5982,63 @@ namespace FuckingAlgorithm {
                 }
                 return res;
             }
+
+            // #797
+            public List<List<int>> AllPathsSourceTarget(int[][] graph) {
+                var res = new List<List<int>>();
+                var path = new List<int> { 0 };
+                int n = graph.Length;
+                void Helper(int begin) {
+                    if (begin == n - 1) {
+                        res.Add(path.ToList());
+                        return;
+                    }
+                    foreach (var node in graph[begin]) {
+                        path.Add(node);
+                        Helper(node);
+                        path.RemoveAt(path.Count - 1);
+                    }
+                }
+                Helper(0);
+                return res;
+            }
+
+            public int GetMaximumGold(int[][] grid) {
+                var dirs = new [] { new [] { 0, 1 }, new [] { 0, -1 }, new int[] { 1, 0 }, new [] {-1, 0 } };
+                int res = 0;
+                int m = grid.Length, n = grid[0].Length;
+                var vis = new bool[m, n];
+
+                void Helper(int i, int j, int sum) {
+                    if (vis[i, j] || grid[i][j] == 0) {
+                        if (sum > res) {
+                            res = sum;
+                        }
+                        return;
+                    }
+                    sum += grid[i][j];
+                    vis[i, j] = true;
+                    foreach (var dir in dirs) {
+                        int tx = i + dir[0];
+                        int ty = j + dir[1];
+                        if (tx < 0 || ty < 0 || tx >= m || ty >= n) continue;
+
+                        Helper(tx, ty, sum);
+
+                    }
+                    sum -= grid[i][j];
+                    vis[i, j] = false;
+                }
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (grid[i][j] != 0) {
+                            Helper(i, j, 0);
+                        }
+                    }
+                }
+                // Helper(1, 2, 0);
+                return res;
+            }
         }
 
         public class DataStructure {
@@ -6321,8 +6378,11 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            algorithm.LetterCasePermutation("a1b2");
+            algorithm.GetMaximumGold(
+                new [] {
+                    new [] { 0, 6, 0 }, new int[] { 5, 8, 7 }, new int[] { 0, 9, 0 }
+                }
+            );
         }
     }
 }
-
