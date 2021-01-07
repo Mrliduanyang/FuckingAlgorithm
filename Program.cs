8ac91d919d68d3435147e01544701179d45480a5
Mrliduanyang <duanyangchn@gmail.com>
2021-01-07 18:48:17 +0800

    2021-01-07

--- Program.cs
+++ Program.cs
@@ -6075,6 +6075,27 @@ namespace FuckingAlgorithm {
                 }
                 return res.ToArray();
             }
+
+            // #456
+            public bool Find132pattern(int[] nums) {
+                if (nums.Length < 3)
+                    return false;
+                var stack = new Stack<int>();
+                int[] min = new int[nums.Length];
+                min[0] = nums[0];
+                for (int i = 1; i < nums.Length; i++)
+                    min[i] = Math.Min(min[i - 1], nums[i]);
+                for (int j = nums.Length - 1; j >= 0; j--) {
+                    if (nums[j] > min[j]) {
+                        while (stack.Count != 0 && stack.Peek() <= min[j])
+                            stack.Pop();
+                        if (stack.Count != 0 && stack.Peek() < nums[j])
+                            return true;
+                        stack.Push(nums[j]);
+                    }
+                }
+                return false;
+            }
         }
 
         public class DataStructure {
@@ -6414,7 +6435,7 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            algorithm.NumsSameConsecDiff(2, 0);
+            algorithm.Find132pattern(new int[] { 3, 5, 0, 3, 4 });
         }
     }
 }
