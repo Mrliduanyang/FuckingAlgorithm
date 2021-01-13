03273fbc3a67a18c702ca5bd1724e9a051cce55f
Mrliduanyang <liduanyangchina@gmail.com>
2021-01-13 22:04:33 +0800

    2021-01-13

--- Program.cs
+++ Program.cs
@@ -6075,6 +6075,41 @@ namespace FuckingAlgorithm {
                 }
                 return res.ToArray();
             }
+
+            // #684
+            public int[] FindRedundantConnection(int[][] edges) {
+                int nodesCount = edges.Length;
+                int[] parent = new int[nodesCount + 1];
+                for (int i = 1; i <= nodesCount; i++) {
+                    parent[i] = i;
+                }
+                void Union(int x, int y) {
+                    int px = Find(x);
+                    int py = Find(y);
+                    if (px == py) {
+                        return;
+                    }
+                    parent[px] = py;
+                }
+
+                int Find(int x) {
+                    if (parent[x] == x) {
+                        return x;
+                    }
+                    return Find(parent[x]);
+                }
+
+                for (int i = 0; i < nodesCount; i++) {
+                    int[] edge = edges[i];
+                    int node1 = edge[0], node2 = edge[1];
+                    if (Find(node1) != Find(node2)) {
+                        Union(node1, node2);
+                    } else {
+                        return edge;
+                    }
+                }
+                return new int[0];
+            }
         }
 
         public class DataStructure {
@@ -6414,7 +6449,7 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            algorithm.NumsSameConsecDiff(2, 0);
+            algorithm.test();
         }
     }
 }
