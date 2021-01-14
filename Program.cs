e9d6de452014a97b2ada22c740008814d317ce30
Mrliduanyang <liduanyangchina@gmail.com>
2021-01-14 15:00:35 +0800

    2021-01-14

--- Program.cs
+++ Program.cs
@@ -6272,6 +6272,18 @@ namespace FuckingAlgorithm {
                 }
                 return sb.ToString();
             }
+
+            // #1018
+            public List<bool> PrefixesDivBy5(int[] A) {
+                var list = new List<bool>();
+                int prefix = 0;
+                int length = A.Length;
+                for (int i = 0; i < length; i++) {
+                    prefix = ((prefix << 1) + A[i]) % 5;
+                    list.Add(prefix == 0);
+                }
+                return list;
+            }
         }
 
         public class DataStructure {
