7383dd7cfaab5ff47aa5e2fb67feb5a2fa5fc85d
Mrliduanyang <duanyangchn@gmail.com>
2021-01-24 10:23:50 +0800

    2021-01-24

--- Program.cs
+++ Program.cs
@@ -6330,6 +6330,20 @@ namespace FuckingAlgorithm {
                 return res;
             }
 
+            // #674
+            public int FindLengthOfLCIS(int[] nums) {
+                int ans = 0;
+                int n = nums.Length;
+                int start = 0;
+                for (int i = 0; i < n; i++) {
+                    if (i > 0 && nums[i] <= nums[i - 1]) {
+                        start = i;
+                    }
+                    ans = Math.Max(ans, i - start + 1);
+                }
+                return ans;
+            }
+
         }
 
         public class DataStructure {
