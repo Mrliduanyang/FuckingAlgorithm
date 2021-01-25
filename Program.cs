d7d5d0eb3b58540017dc74402e07a8a7d0964948
Mrliduanyang <duanyangchn@gmail.com>
2021-01-25 20:01:24 +0800

    2021-01-25

--- Program.cs
+++ Program.cs
@@ -6338,6 +6338,25 @@ namespace FuckingAlgorithm {
                 return ans;
             }
 
+            // #581
+            public int FindUnsortedSubarray(int[] nums) {
+                var stack = new Stack<int>();
+                int l = nums.Length, r = 0;
+                for (int i = 0; i < nums.Length; i++) {
+                    while (stack.Count != 0 && nums[stack.Peek()] > nums[i]) {
+                        l = Math.Min(l, stack.Pop());
+                    }
+                    stack.Push(i);
+                }
+
+                for (int i = nums.Length - 1; i >= 0; i--) {
+                    while (stack.Count != 0 && nums[stack.Peek()] < nums[i]) {
+                        r = Math.Max(r, stack.Pop());
+                    }
+                    stack.Push(i);
+                }
+                return r - l > 0 ? r - l + 1 : 0;
+            }
         }
 
         public class DataStructure {
@@ -6677,7 +6696,6 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            // 2021-01-21
         }
     }
 }
