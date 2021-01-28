5ca241331567f177f14456f0814f16663110b235
Mrliduanyang <liduanyangchina@gmail.com>
2021-01-28 12:51:41 +0800

    2021-01-28

--- Program.cs
+++ Program.cs
@@ -6382,6 +6382,18 @@ namespace FuckingAlgorithm {
                 }
                 return ret;
             }
+
+            // #724
+            public int PivotIndex(int[] nums) {
+                if (nums == null || nums.Length < 3) return -1;
+                int leftSum = 0, rightSum = 0, sum = nums.Sum();
+                for (int i = 0, l = nums.Length; i < l; i++) {
+                    if (leftSum == (sum - leftSum - nums[i]))
+                        return i;
+                    leftSum += nums[i];
+                }
+                return -1;
+            }
         }
 
         public class DataStructure {
