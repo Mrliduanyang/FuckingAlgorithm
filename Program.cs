ac799577f5291fd7876ef8dbc6283eb53d31fcb1
Mrliduanyang <liduanyangchina@gmail.com>
2021-02-04 10:57:48 +0800

    2021-02-04

--- Program.cs
+++ Program.cs
@@ -6554,6 +6554,21 @@ namespace FuckingAlgorithm {
                 //窗口移动到最后，返回长度
                 return right - left;
             }
+
+            // #643
+            public double FindMaxAverage(int[] nums, int k) {
+                int sum = 0;
+                int n = nums.Length;
+                for (int i = 0; i < k; i++) {
+                    sum += nums[i];
+                }
+                int maxSum = sum;
+                for (int i = k; i < n; i++) {
+                    sum = sum - nums[i - k] + nums[i];
+                    maxSum = Math.Max(maxSum, sum);
+                }
+                return 1.0 * maxSum / k;
+            }
         }
 
         public class DataStructure {
