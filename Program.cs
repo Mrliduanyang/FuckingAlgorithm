26fd6a5f07512b90070154c5bbdb6137519c8640
Mrliduanyang <duanyangchn@gmail.com>
2021-02-09 21:18:31 +0800

    2021-02-09

--- Program.cs
+++ Program.cs
@@ -6694,6 +6694,35 @@ namespace FuckingAlgorithm {
                 return false;
             }
 
+            // #992
+            public int SubarraysWithKDistinct(int[] A, int K) {
+                int Helper(int[] A, int K) {
+                    int len = A.Length;
+                    int[] freq = new int[len + 1];
+                    int left = 0;
+                    int right = 0;
+                    int count = 0;
+                    int res = 0;
+                    while (right < len) {
+                        if (freq[A[right]] == 0) {
+                            count++;
+                        }
+                        freq[A[right]]++;
+                        right++;
+
+                        while (count > K) {
+                            freq[A[left]]--;
+                            if (freq[A[left]] == 0) {
+                                count--;
+                            }
+                            left++;
+                        }
+                        res += right - left;
+                    }
+                    return res;
+                }
+                return Helper(A, K) - Helper(A, K - 1);
+            }
         }
 
         public class DataStructure {
