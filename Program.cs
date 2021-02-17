100949da92896e7d2e2147bb0692f51616f0e424
Mrliduanyang <duanyangchn@gmail.com>
2021-02-17 14:44:02 +0800

    2021-02-17

--- Program.cs
+++ Program.cs
@@ -6831,6 +6831,24 @@ namespace FuckingAlgorithm {
                 }
                 return max;
             }
+
+            // #566
+            public int[][] MatrixReshape(int[][] nums, int r, int c) {
+                int m = nums.Length;
+                int n = nums[0].Length;
+                if (m * n != r * c) {
+                    return nums;
+                }
+
+                int[][] ans = new int[r][];
+                for (int i = 0; i < r; i++) {
+                    ans[i] = new int[c];
+                }
+                for (int x = 0; x < m * n; ++x) {
+                    ans[x / c][x % c] = nums[x / n][x % n];
+                }
+                return ans;
+            }
         }
 
         public class DataStructure {
