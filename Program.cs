189d44065c201884c9bf0721fa712d845430fc46
Mrliduanyang <duanyangchn@gmail.com>
2021-02-18 23:43:08 +0800

    2021-02-18

--- Program.cs
+++ Program.cs
@@ -6849,6 +6849,27 @@ namespace FuckingAlgorithm {
                 }
                 return ans;
             }
+
+            // #995
+            public int MinKBitFlips(int[] A, int K) {
+                int n = A.Length;
+                int ans = 0, revCnt = 0;
+                for (int i = 0; i < n; ++i) {
+                    if (i >= K && A[i - K] > 1) {
+                        revCnt ^= 1;
+                        A[i - K] -= 2; // 复原数组元素，若允许修改数组 A，则可以省略
+                    }
+                    if (A[i] == revCnt) {
+                        if (i + K > n) {
+                            return -1;
+                        }
+                        ++ans;
+                        revCnt ^= 1;
+                        A[i] += 2;
+                    }
+                }
+                return ans;
+            }
         }
 
         public class DataStructure {
