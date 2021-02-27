4bd7d91a693c100baee68d3156847ba512a2e7d9
Mrliduanyang <duanyangchn@gmail.com>
2021-02-27 22:41:08 +0800

    2021-02-27

--- Program.cs
+++ Program.cs
@@ -7096,6 +7096,22 @@ namespace FuckingAlgorithm {
                 return ret;
             }
 
+            // #788
+            public int RotatedDigits(int N) {
+                bool Helper(int n, bool flag) {
+                    if (n == 0) return flag;
+
+                    int d = n % 10;
+                    if (d == 3 || d == 4 || d == 7) return false;
+                    if (d == 0 || d == 1 || d == 8) return Helper(n / 10, flag);
+                    return Helper(n / 10, true);
+                }
+                int ans = 0;
+                for (int n = 1; n <= N; ++n)
+                    if (Helper(n, false)) ans++;
+                return ans;
+            }
+
         }
 
         public class DataStructure {
