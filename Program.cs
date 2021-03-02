c12436494fc9eb66c35f731c5e91448b48804ca3
Mrliduanyang <duanyangchn@gmail.com>
2021-03-02 22:54:04 +0800

    2021-03-02

--- Program.cs
+++ Program.cs
@@ -7151,6 +7151,22 @@ namespace FuckingAlgorithm {
                     return sums[j + 1] - sums[i];
                 }
             }
+
+            // #799
+            public double ChampagneTower(int poured, int query_row, int query_glass) {
+                var A = new double[102, 102];
+                A[0, 0] = (double) poured;
+                for (int r = 0; r <= query_row; ++r) {
+                    for (int c = 0; c <= r; ++c) {
+                        double q = (A[r, c] - 1.0) / 2.0;
+                        if (q > 0) {
+                            A[r + 1, c] += q;
+                            A[r + 1, c + 1] += q;
+                        }
+                    }
+                }
+                return Math.Min(1, A[query_row, query_glass]);
+            }
         }
 
         public class DataStructure {
@@ -7555,7 +7571,6 @@ namespace FuckingAlgorithm {
         }
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            
         }
     }
 }
