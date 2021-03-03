7331621ae75ea7b02502d742d4f408cddd541702
Mrliduanyang <duanyangchn@gmail.com>
2021-03-03 14:57:28 +0800

    2021-03-03

--- Program.cs
+++ Program.cs
@@ -7180,6 +7180,20 @@ namespace FuckingAlgorithm {
                 return new string(res);
             }
 
+            // #338
+            public int[] CountBits(int num) {
+                var res = new int[num + 1];
+                res[0] = 0;
+                for (int i = 0; i <= num; i++) {
+                    if (i % 2 == 1) {
+                        res[i] = res[i - 1] + 1;
+                    } else {
+                        res[i] = res[i / 2];
+                    }
+                }
+                return res;
+            }
+
         }
 
         public class DataStructure {
