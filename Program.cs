e12563af1b32922c40829f82a238bdbbd5453669
Mrliduanyang <duanyangchn@gmail.com>
2021-03-07 13:08:24 +0800

    2021-03-07

--- Program.cs
+++ Program.cs
@@ -7424,6 +7424,27 @@ namespace FuckingAlgorithm {
                 long N = n;
                 return N >= 0 ? QuickMul(x, N) : 1.0 / QuickMul(x, -N);
             }
+
+            // #91
+            public int NumDecodings(string s) {
+                if (s[0] == '0') return 0;
+                var dp = new int[s.Length + 1];
+                dp[0] = dp[1] = 1;
+                for (int i = 1; i < s.Length; i++) {
+                    if (s[i] == '0') {
+                        if (s[i - 1] == '1' || s[i - 1] == '2') {
+                            dp[i + 1] = dp[i - 1];
+                        } else {
+                            return 0;
+                        }
+                    } else if (s[i - 1] == '1' || (s[i - 1] == '2' && s[i] >= '1' && s[i] <= '6')) {
+                        dp[i + 1] = dp[i] + dp[i - 1];
+                    } else {
+                        dp[i + 1] = dp[i];
+                    }
+                }
+                return dp.Last();
+            }
         }
 
         public class DataStructure {
@@ -7830,7 +7851,7 @@ namespace FuckingAlgorithm {
 
         static void Main(string[] args) {
             var algorithm = new Algorithm();
-            algorithm.CompareVersion("1.01", "1.001");
+            algorithm.NumDecodings("2101");
         }
     }
 }
