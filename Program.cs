96fc6f6ddfd17473442a7f322076eb4e4e01c22b
Mrliduanyang <duanyangchn@gmail.com>
2021-03-08 08:57:50 +0800

    2021-03-08

--- Program.cs
+++ Program.cs
@@ -7596,6 +7596,59 @@ namespace FuckingAlgorithm {
                 }
                 return n + 1;
             }
+
+            // #297
+            public class Codec {
+                public string serialize(TreeNode root) {
+                    if (root == null) return string.Empty;
+
+                    var res = new List<string>();
+                    var queue = new Queue<TreeNode>();
+
+                    queue.Enqueue(root);
+
+                    while (queue.Count != 0) {
+                        var node = queue.Dequeue();
+
+                        if (node == null) {
+                            res.Add("null");
+                            continue;
+                        }
+                        res.Add(node.val.ToString());
+
+                        queue.Enqueue(node.left);
+                        queue.Enqueue(node.right);
+                    }
+
+                    return string.Join(",", res);
+                }
+
+                public TreeNode deserialize(string data) {
+                    if (string.IsNullOrEmpty(data)) return null;
+
+                    string[] nodes = data.Split(',');
+                    var root = new TreeNode(int.Parse(nodes[0]));
+                    var queue = new Queue<TreeNode>();
+                    queue.Enqueue(root);
+                    int idx = 1;
+                    while (queue.Count != 0) {
+                        var node = queue.Dequeue();
+                        node.left = nodes[idx] == "null" ? null : new TreeNode(int.Parse(nodes[idx]));
+                        if (node.left != null) {
+                            queue.Enqueue(node.left);
+                        }
+                        idx++;
+
+                        node.right = nodes[idx] == "null" ? null : new TreeNode(int.Parse(nodes[idx]));
+                        if (node.right != null) {
+                            queue.Enqueue(node.right);
+                        }
+                        idx++;
+                    }
+                    return root;
+                }
+            }
+
         }
 
         public class DataStructure {
