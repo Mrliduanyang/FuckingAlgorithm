e1fa4eddffdfeff3ccd1f381a862a53cd292c2f2
Mrliduanyang <duanyangchn@gmail.com>
2021-03-17 18:18:13 +0800

    2021-03-16

--- Program.cs
+++ Program.cs
@@ -8346,6 +8346,28 @@ namespace FuckingAlgorithm {
                 }
                 Reverse(0, s.Length - 1);
             }
+
+            // #115
+            public int NumDistinct(string s, string t) {
+                int m = s.Length, n = t.Length;
+                if (m < n) {
+                    return 0;
+                }
+                var dp = new int[m + 1, n + 1];
+                for (int i = 0; i <= m; i++) {
+                    dp[i, n] = 1;
+                }
+                for (int i = m - 1; i >= 0; i--) {
+                    for (int j = n - 1; j >= 0; j--) {
+                        if (s[i] == t[j]) {
+                            dp[i, j] = dp[i + 1, j + 1] + dp[i + 1, j];
+                        } else {
+                            dp[i, j] = dp[i + 1, j];
+                        }
+                    }
+                }
+                return dp[0, 0];
+            }
         }
 
         public class DataStructure {
