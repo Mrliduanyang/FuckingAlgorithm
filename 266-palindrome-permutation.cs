46db6718ea6006e8bfbba01a662572404cce9fc7
Mrliduanyang <duanyangchn@gmail.com>
2021-04-05 20:46:43 +0800

    2021-04-05

---
+++ 266-palindrome-permutation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public class Solution {
+    public bool CanPermutePalindrome(string s) {
+        var dict = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; ++i) {
+            dict[s[i]] = dict.GetValueOrDefault(s[i], 0) + 1;
+        }
+
+        int count = 0;
+        foreach (var (key, val) in dict) {
+            count += (val % 2);
+        }
+
+        return count <= 1;
+    }
+}
