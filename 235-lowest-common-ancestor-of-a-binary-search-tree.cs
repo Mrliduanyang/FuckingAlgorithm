b209e2b12a1643d7c09c41213e7307c54dacdca2
Mrliduanyang <duanyangchn@gmail.com>
2021-04-12 20:26:33 +0800

    2021-04-12

--- 235-lowest-common-ancestor-of-a-binary-search-tree.cs
+++ 235-lowest-common-ancestor-of-a-binary-search-tree.cs
@@ -9,12 +9,12 @@
  */
 public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode Helper(TreeNode root, TreeNode p, TreeNode q) {
-            if (p.val < root.val && q.val < root.val)
-                return Helper(root.left, p, q);
-            if (p.val > root.val && q.val > root.val)
-                return Helper(root.right, p, q);
-            return root;
+        TreeNode Helper(TreeNode node, TreeNode p, TreeNode q) {
+            if (p.val < node.val && q.val < node.val)
+                return Helper(node.left, p, q);
+            if (p.val > node.val && q.val > node.val)
+                return Helper(node.right, p, q);
+            return node;
         }
 
         return Helper(root, p, q);
--- 236-lowest-common-ancestor-of-a-binary-tree.cs
+++ 236-lowest-common-ancestor-of-a-binary-tree.cs
@@ -11,10 +11,10 @@ public class Solution {
     public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode Helper(TreeNode node, TreeNode p, TreeNode q) {
             if (node == null || node == p || node == q) return node;
-            var l = Helper(node.left, p, q);
-            var r = Helper(node.right, p, q);
-            if (l != null && r != null) return node;
-            return l == null ? r : l;
+            var left = Helper(node.left, p, q);
+            var right = Helper(node.right, p, q);
+            if (left != null && right != null) return node;
+            return left == null ? right : left;
         }
 
         return Helper(root, p, q);
