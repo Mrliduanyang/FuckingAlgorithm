fcd8b0a481bfe84351db55cd6ec2a442eff770ae
Mrliduanyang <duanyangchn@gmail.com>
2021-04-19 10:34:14 +0800

    2021-04-19

--- 26-remove-duplicates-from-sorted-array.cs
+++ 26-remove-duplicates-from-sorted-array.cs
@@ -3,13 +3,12 @@ public class Solution {
         var len = nums.Length;
         if (len == 0) return 0;
         var curr = 0;
-        for (var i = 1; i < len; i++)
-        {
-            if (nums[i] > nums[curr])
-            {
+        for (var i = 1; i < len; i++) {
+            if (nums[i] > nums[curr]) {
                 nums[++curr] = nums[i];
             }
         }
+
         return curr + 1;
     }
 }
