45be130fc56f971e33572d73322e09a1250092d3
Mrliduanyang <duanyangchn@gmail.com>
2021-04-24 14:43:16 +0800

    2021-04-24

--- 377-combination-sum-iv.cs
+++ 377-combination-sum-iv.cs
@@ -1,21 +1,15 @@
 public class Solution {
     public int CombinationSum4(int[] nums, int target) {
         var dp = new int[target + 1];
-
-        int Helper(int target) {
-            if (target == 0) return 1;
-            if (dp[target] >= 0) return dp[target];
-            var count = 0;
-            for (var i = 0; i < nums.Length; i++)
-                if (target >= nums[i])
-                    count += Helper(target - nums[i]);
-            dp[target] = count;
-            return count;
+        dp[0] = 1;
+        for (var i = 1; i <= target; ++i) {
+            foreach (var num in nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
         }
 
-        Array.Sort(nums);
-        for (var i = 0; i < dp.Length; i++) dp[i] = -1;
-        var count = Helper(target);
-        return count;
+        return dp[target];
     }
 }
