e84cbec77a5e4cf9ad077e6a9621f67cfc72e24f
李端阳 <duanyangchn@gmail.com>
2021-05-01 21:50:33 +0800

    2021-05-01

--- 690-employee-importance.cs
+++ 690-employee-importance.cs
@@ -7,16 +7,18 @@ class Employee {
 }
 */
 
-internal class Solution {
+class Solution {
     public int GetImportance(IList<Employee> employees, int id) {
-        Dictionary<int, Employee> dic = new Dictionary<int, Employee>();
-        foreach (var item in employees) dic.Add(item.id, item);
-        return GetRes(dic, id);
-    }
-
-    public int GetRes(Dictionary<int, Employee> dic, int id) {
-        int res = dic[id].importance;
-        foreach (var item in dic[id].subordinates) res += GetRes(dic, item);
+        var dict = employees.ToDictionary(employee => employee.id, employee => employee);
+        var res = 0;
+        void Helper(int id){
+            var employee = dict[id];
+            res += employee.importance;
+            foreach(var subordinate in employee.subordinates){
+                Helper(subordinate);
+            }
+        }
+        Helper(id);
         return res;
     }
 }
