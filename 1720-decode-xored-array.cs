f4ae1c3468273a0cb77bd70356ddbbdbd8aec904
Mrliduanyang <duanyangchn@gmail.com>
2021-05-06 15:12:53 +0800

    2021-05-06

--- 1720-decode-xored-array.cs
+++ 1720-decode-xored-array.cs
@@ -1,9 +1,9 @@
 public class Solution {
     public int[] Decode(int[] encoded, int first) {
-        int n = encoded.Length + 1;
-        int[] arr = new int[n];
+        var n = encoded.Length + 1;
+        var arr = new int[n];
         arr[0] = first;
-        for (int i = 1; i < n; i++) {
+        for (var i = 1; i < n; i++) {
             arr[i] = arr[i - 1] ^ encoded[i - 1];
         }
         return arr;
