8a38e0760d138094f16c592759838367fecbd5ea
Mrliduanyang <duanyangchn@gmail.com>
2021-05-17 18:44:01 +0800

    2021-05-17

--- pattern-matching-lcci.cs
+++ pattern-matching-lcci.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Text;
 
 public class Solution {
     public bool PatternMatching(string pattern, string value) {
         int countA = 0, countB = 0;
-        var startWithA = pattern.StartsWith('a');
+        if (!pattern.StartsWith('a')) {
+            pattern = pattern.Replace('a', 'c');
+            pattern = pattern.Replace('b', 'a');
+            pattern = pattern.Replace('c', 'b');
+        }
 
         foreach (var ch in pattern) {
             if (ch == 'a') {
@@ -14,29 +19,32 @@ public class Solution {
             }
         }
 
+        if (value.Length == 0) {
+            return countB == 0;
+        }
+
         var n = value.Length;
 
-        for (var lenA = 0; lenA < n; ++lenA) {
+        for (var lenA = 0; lenA <= n; ++lenA) {
             var patA = value[..lenA];
-            for (var lenB = 0; lenB < n; ++lenB) {
+            for (var lenB = 0; lenB <= n; ++lenB) {
                 if (countA * lenA + countB * lenB == n) {
                     var patB = "";
                     var bIdx = 1;
                     while (bIdx < pattern.Length) {
-                        if (pattern[bIdx] == (startWithA ? 'b' : 'a')) {
+                        if (pattern[bIdx] == 'b') {
                             patB = value.Substring((lenA * bIdx), lenB);
                             break;
                         }
+
+                        ++bIdx;
                     }
+
+                    Console.WriteLine($"{patA},{patB}");
 
                     var newValue = new StringBuilder();
                     foreach (var ch in pattern) {
-                        if (ch == 'a') {
-                            newValue.Append(patA);
-                        }
-                        else {
-                            newValue.Append(patB);
-                        }
+                        newValue.Append(ch == 'a' ? patA : patB);
                     }
 
                     if (newValue.ToString() == value) {
