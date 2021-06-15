8d24047a2132bd15d15f4ce6d8672a8edc17b0dc
Mrliduanyang <duanyangchn@gmail.com>
2021-06-15 16:49:22 +0800

    2021-06-15

---
+++ 852-peak-index-in-a-mountain-array.cs
@@ -0,0 +1,17 @@
+﻿public class Solution {
+    public int PeakIndexInMountainArray(int[] arr) {
+        var n = arr.Length;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            var mid = left + (right - left) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
