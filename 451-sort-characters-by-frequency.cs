cc17901dd0b1825e1e621f9c3ac24e41257d6de6
Mrliduanyang <duanyangchn@gmail.com>
2021-07-03 19:54:36 +0800

    2021-07-03

--- 451-sort-characters-by-frequency.cs
+++ 451-sort-characters-by-frequency.cs
@@ -1,12 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 public class Solution {
     public string FrequencySort(string s) {
-        var res = new StringBuilder();
         var dict = new Dictionary<char, int>();
         foreach (var ch in s) dict[ch] = dict.GetValueOrDefault(ch, 0) + 1;
-        var tmp = dict.OrderByDescending(x => x.Value);
-        foreach (var (key, val) in tmp)
-            for (var i = 0; i < val; i++)
-                res.Append(key);
+        var maxFreq = dict.Values.Max();
+        var buckets = new StringBuilder[maxFreq + 1];
+        for (var i = 0; i <= maxFreq; ++i) {
+            buckets[i] = new StringBuilder();
+        }
+
+        foreach (var (key, val) in dict) {
+            for (var i = 0; i < val; ++i) {
+                buckets[val].Append(key);
+            }
+        }
+
+        var res = new StringBuilder();
+        for (var i = maxFreq; i > 0; --i) {
+            var bucket = buckets[i];
+            res.Append(bucket.ToString());
+        }
+
         return res.ToString();
     }
 }
