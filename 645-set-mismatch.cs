6da50164e9a49d47bd8624620c67ead35da6ec07
Mrliduanyang <duanyangchn@gmail.com>
2021-07-04 13:29:11 +0800

    2021-07-04

---
+++ 645-set-mismatch.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public int[] FindErrorNums(int[] nums) {
+        var len = nums.Length;
+        var total = new HashSet<int>(nums).Sum();
+        return new[] {nums.Sum() - total, (1 + len) * len / 2 - total};
+    }
+}
