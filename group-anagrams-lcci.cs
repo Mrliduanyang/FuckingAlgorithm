2247991701a3561adbe5c14cd774719676419da6
Mrliduanyang <duanyangchn@gmail.com>
2021-07-18 14:04:09 +0800

    2021-07-18

--- group-anagrams-lcci.cs
+++ group-anagrams-lcci.cs
@@ -1,22 +1,32 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 public class Solution {
-    public List<List<string>> GroupAnagrams(string[] strs) {
-        if (strs.Length == 0) {
-            return new List<List<string>>();;
-        }
-
+    public List<IList<string>> GroupAnagrams(string[] strs) {
         var dict = new Dictionary<string, List<string>>();
         foreach (var str in strs) {
-            var tmp = string.Join("", str.OrderBy(x => x));
-            if (!dict.ContainsKey(tmp)) {
-                dict[tmp] = new List<string>();
+            var count = new int[26];
+            foreach (var ch in str) {
+                count[ch - 'a']++;
             }
 
-            dict[tmp].Add(str);
+            var sb = new StringBuilder();
+            for (var i = 0; i < 26; ++i) {
+                if (count[i] > 0) {
+                    sb.Append('a' + i);
+                    sb.Append(count[i]);
+                }
+            }
+
+            var key = sb.ToString();
+            if (!dict.ContainsKey(key)) {
+                dict[key] = new List<string>();
+            }
+
+            dict[key].Add(str);
         }
-        
-        return dict.Values.ToList();
+
+        return new List<IList<string>>(dict.Values);
     }
 }
