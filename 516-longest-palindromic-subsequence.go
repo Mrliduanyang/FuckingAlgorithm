0921c432d910ed45b35643d239761c7057e314c6
Mrliduanyang <duanyangchn@gmail.com>
2021-08-12 14:07:36 +0800

    2021-08-12

---
+++ 516-longest-palindromic-subsequence.go
@@ -0,0 +1,26 @@
+package main
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+func longestPalindromeSubseq(s string) int {
+	n := len(s)
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
+	for i := n - 1; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][n-1]
+}
