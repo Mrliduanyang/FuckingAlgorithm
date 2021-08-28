02dbf4816b3a4e47e5c320addfdcbb1b57023388
Mrliduanyang <duanyangchn@gmail.com>
2021-08-28 14:15:39 +0800

    2021-08-28

---
+++ 1480-running-sum-of-1d-array.go
@@ -0,0 +1,11 @@
+package main
+
+func runningSum(nums []int) []int {
+	n := len(nums)
+	res := make([]int, n)
+	res[0] = nums[0]
+	for i := 1; i < n; i++ {
+		res[i] = res[i-1] + nums[i]
+	}
+	return res
+}
