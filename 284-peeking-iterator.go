47453295436264fe566144865567939cfea646b5
Mrliduanyang <duanyangchn@gmail.com>
2021-10-05 18:50:38 +0800

    2021-10-05

---
+++ 284-peeking-iterator.go
@@ -0,0 +1,42 @@
+/*   Below is the interface for Iterator, which is already defined for you.
+ *
+ *   type Iterator struct {
+ *
+ *   }
+ *
+ *   func (this *Iterator) hasNext() bool {
+ *		// Returns true if the iteration has more elements.
+ *   }
+ *
+ *   func (this *Iterator) next() int {
+ *		// Returns the next element in the iteration.
+ *   }
+ */
+package main
+
+type PeekingIterator struct {
+	iter     *Iterator
+	_hasNext bool
+	_next    int
+}
+
+func Constructor(iter *Iterator) *PeekingIterator {
+	return &PeekingIterator{iter, iter.hasNext(), iter.next()}
+}
+
+func (this *PeekingIterator) hasNext() bool {
+	return this._hasNext
+}
+
+func (this *PeekingIterator) next() int {
+	ret := this._next
+	this._hasNext = this.iter.hasNext()
+	if this._hasNext {
+		this._next = this.iter.next()
+	}
+	return ret
+}
+
+func (this *PeekingIterator) peek() int {
+	return this._next
+}
