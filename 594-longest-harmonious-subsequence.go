49f9a960070a1d8b8e981338f48306ec8a858d29
Mrliduanyang <duanyangchn@gmail.com>
2021-11-20 19:47:59 +0800

    2021-11-20

---
+++ 594-longest-harmonious-subsequence.go
@@ -0,0 +1,14 @@
+package main
+
+func findLHS(nums []int) (ans int) {
+	cnt := map[int]int{}
+	for _, num := range nums {
+		cnt[num]++
+	}
+	for num, c := range cnt {
+		if c1 := cnt[num+1]; c1 > 0 && c+c1 > ans {
+			ans = c + c1
+		}
+	}
+	return
+}
